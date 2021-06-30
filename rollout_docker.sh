bcfa079c3217f3e7c59264989898278a808469fe
TeeWallz <thomas.r.waller@gmail.com>
2021-06-30 10:19:04 +1000

    rollout_docker.sh code cleanup

--- rollout_docker.sh
+++ rollout_docker.sh
@@ -1,12 +1,13 @@
+#!/bin bash
+
+#Cifs for fileshare mount and docker obviously
 sudo install cifs-common docker.io
 
-/etc/fstab
+# Add Media and main SHARE as mounts/etc/fstab
 printf "192.168.0.100:/mnt/Raid_Pool/SHARE /mnt/share nfs rw,hard,intr,rsize=8192,wsize=8192,timeo=14  0 0\n" >> /etc/fstab
 printf "192.168.0.100:/mnt/Raid_Pool/SHARE/MEDIA /mnt/media nfs rw,hard,intr,rsize=8192,wsize=8192,timeo=14  0 0\n" >> /etc/fstab
-
 mkdir -p /mnt/share
 mkdir -p /mnt/media
-
 sudo mount -a
 
 
