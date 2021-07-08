00d5b3bf3aacf5bcf44698efb3c294a43bf32666
TeeWallz <thomas.r.waller@gmail.com>
2021-07-09 08:25:39 +1000

    Update rollout_docker_on_host.sh
    Fixed rollout script

--- rollout_docker_on_host.sh
+++ rollout_docker_on_host.sh
@@ -1,7 +1,7 @@
 #!/bin bash
 
 #Cifs for fileshare mount and docker obviously
-sudo install cifs-common docker.io
+sudo apt install nfs-common docker.io
 
 # Add Media and main SHARE as mounts/etc/fstab
 printf "192.168.0.100:/mnt/Raid_Pool/SHARE /mnt/share nfs rw,hard,intr,rsize=8192,wsize=8192,timeo=14  0 0\n" >> /etc/fstab
